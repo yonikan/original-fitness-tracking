ceabb4dc9dde394706dddba224d9bc1f3d49cc5c
kangun, jonatan <Jonatan.Kangun@verint.com>
2018-08-26 22:01:58 +0300

    init from the maximilian original.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBMcWYIoQMRYaekdCxqn7EOjelN8R4gaTc",
+    authDomain: "original-fitness-tracking.firebaseapp.com",
+    databaseURL: "https://original-fitness-tracking.firebaseio.com",
+    projectId: "original-fitness-tracking",
+    storageBucket: "original-fitness-tracking.appspot.com",
+    messagingSenderId: "439680209633"
+  }
+};
